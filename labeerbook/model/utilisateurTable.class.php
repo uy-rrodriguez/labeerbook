ca3423a1c26147dfadc4d23158ded5ade9817081
Castillo Quentin <uapv1403752@adm-2w22a-alex.univ-avignon.fr>
2016-11-08 16:26:11 +0100

    Modifs by Quentin

--- labeerbook/model/utilisateurTable.class.php
+++ labeerbook/model/utilisateurTable.class.php
@@ -15,6 +15,19 @@ public static function getUserByLoginAndPass($login,$pass){
 	return $user; 
 }
 
+public static function getUserById($id){
+	$em = dbconnection::getInstance()->getEntityManager() ;
+
+	$userRepository = $em->getRepository('utilisateur');
+	$user = $userRepository->findOneBy(array('id' => $id));	
+
+	return $user; 
+}
+
+
+public static function getUsers(){
+	$em = dbconnection::getInstance()->getEntityManager() ;
+}
 
 }
 
