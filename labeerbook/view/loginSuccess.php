2dad3b49a7421688458702f2a43a2b6ff8b0ca8f
Ectherelion <quentin.castillo@hotmail.fr>
2016-11-13 20:07:22 +0100

    Update loginSuccess.php

--- labeerbook/view/loginSuccess.php
+++ labeerbook/view/loginSuccess.php
@@ -5,6 +5,7 @@ Vous êtes désormais connecté, <?php echo $context->getSessionAttribute("user"
 <ul>
 	<li><a href="?action=showMessage&idUtilisateur=1">Show messages</a></li>
 	<li><a href="?action=showChats">Show chats</a></li>
+	<li><a href="?action=showUsers">Show all users</a></li>
 </ul>
 
 <button onclick="logout()">Logout</button>
