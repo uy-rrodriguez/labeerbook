c16885c28cffec70e7fa3c790c6aac13ca32a7e4
Ricardo <rr.ricci@gmail.com>
2016-11-14 18:48:56 +0100

    Modif profil avant rendu

--- labeerbook/view/loginSuccess.php
+++ labeerbook/view/loginSuccess.php
@@ -1,13 +1,4 @@
-Vous êtes désormais connecté, <?php echo $context->getSessionAttribute("user")->identifiant; ?>.
-
-<br />
-
-<ul>
-	<li><a href="?action=showMessage&idUtilisateur=1">Show messages</a></li>
-	<li><a href="?action=showChats">Show chats</a></li>
-	<li><a href="?action=showUsers">Show all users</a></li>
-</ul>
-
-<button onclick="logout()">Logout</button>
-
-<?php include($nameApp."/view/templates/profil.php"); ?>
+<?php
+	$userProfil = $context->getSessionAttribute("user");
+	include($nameApp."/view/templates/profil.php");
+?>
--- labeerbook/view/templates/profil.php
+++ labeerbook/view/templates/profil.php
@@ -6,22 +6,25 @@
 	</div>
 
 	<div class="col-xs-12 col-sm-4">
-		<p>Echterelion</p>
-		<p>Quentin CASTILLO</p>
-		<p>Statut > online</p>
+		<p><?php echo $userProfil->identifiant; ?></p>
+		<p><?php echo $userProfil->prenom . " " . $userProfil->nom; ?></p>
 		<p>
-			<svg xmlns="http://www.w3.org/2000/svg">
-				<circle cx="50" cy="50" r="20" fill="green" />
+			Statut
+			<svg xmlns="http://www.w3.org/2000/svg" style="height: 40px; width: 40px;">
+				<circle cx="20" cy="20" r="20" fill="green" />
 			</svg>
 		</p>
 	</div>
 
 	<div class="col-xs-12 col-sm-4">
-		<ul>
-			<li><a href="#">Amis</a></li>
-			<li><a href="#">Chat</a></li>
-			<li><a href="#">Accueil</a></li>
-			<li><a href="#">Deconnexion</a></li>
+		<div class="list-group">
+			<a class="list-group-item" href="?action=showMessage&idUtilisateur=1">Show messages</a>
+			<a class="list-group-item" href="?action=showChats">Show chats</a>
+			<a class="list-group-item" href="?action=showUsers">Show all users</a>
+			<a class="list-group-item disabled" href="#">Amis</a>
+			<a class="list-group-item disabled" href="#">Chat</a>
+			<a class="list-group-item disabled" href="#">Accueil</a>
+			<a class="list-group-item" href="#" onclick="logout()">Deconnexion</a>
 		</ul>
 	</div>
 </div>
