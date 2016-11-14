cb6faab71bfa8bd5daf68eb6a81be9ccc36429c7
Castillo Quentin <uapv1403752@adm-2w22a-alex.univ-avignon.fr>
2016-11-14 17:11:58 +0100

    Debut bootstrap

--- labeerbook/view/layout.php
+++ labeerbook/view/layout.php
@@ -2,12 +2,15 @@
 <html lang="fr">
 <head>
 	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
 	<title>La Beer Book</title>
 	
 	<link rel="stylesheet" type="text/css" href="style.css">
-	
-	<script type="text/javascript" src="static/js/jquery-3.1.1.min.js"></script>
-	<script type="text/javascript" src="static/js/ajax.js"></script>
+	<link href="static/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+
+
 </head>
 
 <body>
@@ -16,5 +19,38 @@
 	<div id="contenu" style="position: relative;">
 		<?php include($template_view); ?>
 	</div>
+
+	 <div class="jumbotron text-center">
+	  <h1>My First Bootstrap Page</h1>
+	  <p>Resize this responsive page to see the effect!</p>
+	</div>
+	<div class="container">
+	  <div class="row">
+	    <div class="col-sm-4">
+	      <h3>Column 1</h3>
+	      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+	      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+	    </div>
+	    <div class="col-sm-4">
+	      <h3>Column 2</h3>
+	      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+	      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+	    </div>
+	    <div class="col-sm-4">
+	      <h3>Column 3</h3>
+	      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+	      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+	    </div>
+	  </div>
+	</div>
+
+
+
+	<!-- Scripts -->
+	<script type="text/javascript" src="static/js/jquery-3.1.1.min.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="static/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+
+	<script type="text/javascript" src="static/js/ajax.js"></script>
 </body>
 </html>
