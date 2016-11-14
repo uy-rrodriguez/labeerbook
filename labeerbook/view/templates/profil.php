1b94cec751ecabef9d8b96449eb1c61971f678f3
Ricardo <rr.ricci@gmail.com>
2016-11-14 18:03:57 +0100

    Premieres modifs pour le profil

---
+++ labeerbook/view/templates/profil.php
@@ -0,0 +1,27 @@
+<?php
+?>
+<div id="profil" class="row">
+	<div class="col-xs-12 col-sm-4">
+		<img id="img-profil" src="static/img/profil.png">
+	</div>
+
+	<div class="col-xs-12 col-sm-4">
+		<p>Echterelion</p>
+		<p>Quentin CASTILLO</p>
+		<p>Statut > online</p>
+		<p>
+			<svg xmlns="http://www.w3.org/2000/svg">
+				<circle cx="50" cy="50" r="20" fill="green" />
+			</svg>
+		</p>
+	</div>
+
+	<div class="col-xs-12 col-sm-4">
+		<ul>
+			<li><a href="#">Amis</a></li>
+			<li><a href="#">Chat</a></li>
+			<li><a href="#">Accueil</a></li>
+			<li><a href="#">Deconnexion</a></li>
+		</ul>
+	</div>
+</div>
