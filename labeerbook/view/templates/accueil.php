3b08f7c6e12c429d896e57242add3b263def7f4e
Castillo Quentin <uapv1403752@adm-2w22a-alex.univ-avignon.fr>
2016-12-12 14:19:34 +0100

    Rendu TP3

--- labeerbook/view/templates/accueil.php
+++ labeerbook/view/templates/accueil.php
@@ -1,11 +1,30 @@
-<div id = "messAccueil" class = "row" >
-	<div class = "col-xs-3">
-		<!-- Afficher Photo -->
-		blabla
+<?php for ($i = 0; $i < 10; $i++) { ?>
+<div id = "message" >
+
+	<div id = "messAccueil" class = "row" >
+		<div class = "col-xs-3">
+			<div id="img-profil-container">
+	        <img id="img-profil" class="img img-responsive" src="static/img/user-1.png">
+	        <img id="img-profil-biere" class="img img-responsive" src="static/img/beer-1.png">
+	    </div>
+		</div>
+
+		<div class = "col-xs-9" >
+			<b>Quentin Castillo : </b><br>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+		</div>
 	</div>
 
-	<div class = "col-xs-6">
-		<!-- Afficher message -->
-		blabla
+
+	<div id="shareLike" class ="row">
+		<div class="col-xs-12">
+			Partager
+			<img href="" src="static/img/share.png">
+			<img href="" src="static/img/like.png">
+		</div>
 	</div>
+
 </div>
+<?php } ?>
+
