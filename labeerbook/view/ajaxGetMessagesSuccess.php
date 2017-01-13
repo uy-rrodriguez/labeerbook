0563c71cede805681ca6f08a1bb6fa86e3bca265
Ricardo <rr.ricci@gmail.com>
2017-01-13 05:47:07 +0100

    Correction affichage messages login. Unification d'affichage de messages

--- labeerbook/view/ajaxGetMessagesSuccess.php
+++ labeerbook/view/ajaxGetMessagesSuccess.php
@@ -6,39 +6,8 @@
 
 
 	foreach ($allMessages as $message) {
-
-        // Image à utiliser si la photo n'est pas trouvé. On récupère une photo aléatoire
-        $imgOnError = "static/img/def-avatars/user (" . ($message->emetteur->id % 15) . ").png";
+        $context->setSessionAttribute("profilMessage", $message);
+        include("templates/message.php");
+    }
 ?>
-
-
-<div id = "message" >
-
-	<div id = "messAccueil" class = "row" >
-		<div class = "col-xs-3">
-			<img class="img" src="<?php echo $AVATAR_FOLDER . "/" . $message->emetteur->avatar; ?>"
-                alt="Image de profil de l'émetteur"
-                onerror="this.onerror=null; this.src='<?php echo $imgOnError; ?>';">
-		</div>
-
-		<div class = "col-xs-9" >
-			<b><?php echo $message->emetteur->identifiant; ?> : </b><br>
-			<?php echo $message->post->texte; ?>
-		</div>
-	</div>
-
-
-	<div id="shareLike" class ="row">
-		<div class="col-xs-12">
-			Partager
-			<img href="" src="static/img/share.png">
-			<img href="" src="static/img/like.png">
-		</div>
-	</div>
-
-</div>
-
-
-
-<?php } ?>
 
---
+++ labeerbook/view/templates/message.php
@@ -0,0 +1,46 @@
+<?php
+    $m = $context->getSessionAttribute("profilMessage");
+
+    // Le dossier avec les uploads se trouve dans une classe de configuration
+    $AVATAR_FOLDER = config::AVATAR_FOLDER;
+
+    // Hack pour les messages qui n'ont pas assigné un émetteur
+    $emetteur = $m->emetteur;
+    if (! $emetteur) {
+        $emetteur = new utilisateur();
+        $emetteur->id = 0;
+        $emetteur->identifiant = "sans identifiant";
+        $emetteur->avatar = "";
+    }
+
+
+    $avatar = $AVATAR_FOLDER . "/" . $emetteur->avatar;
+
+    // Image à utiliser si la photo n'est pas trouvé. On récupère une photo aléatoire
+    $imgOnError = "static/img/def-avatars/user (" . ($emetteur->id % 15) . ").png";
+?>
+
+        <div class="row message">
+            <div class="col-xs-3 msg-photo">
+                <img class="img" src="<?php echo $avatar; ?>"
+                    alt="Image de profil de l'émetteur"
+                    onerror="this.onerror=null; this.src='<?php echo $imgOnError; ?>';">
+            </div>
+
+            <div class="col-xs-9 msg-contenu">
+                <div class="msg-emetteur"><?php echo $emetteur->identifiant; ?></div>
+                <div class="msg-texte">
+                    <pre><?php echo strip_tags($m->post->texte, FILTER_SANITIZE_STRING); ?></pre>
+                </div>
+            </div>
+
+            <div class="msg-share-like">
+                <div class="btn btn-default msg-btn msg-share">
+                    Partager <img href="" src="static/img/share.png">
+                </div>
+                <div class="btn btn-default msg-btn msg-like">
+                    <?php echo ($m->aime != "" ? $m->aime : 0); ?>
+                    <img href="" src="static/img/like.png">
+                </div>
+            </div>
+        </div>
